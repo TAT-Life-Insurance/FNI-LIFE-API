b0af05b60754553d3733e44d8197d7d50e4af6bd
ASUS <ASUS@DESKTOP-37IOB4I>
2019-10-31 14:33:02 +0630

    Initial Commit

---
+++ FNILDEMOAPI/src/main/java/com/ace/demoapi/FnildemoapiApplication.java
@@ -0,0 +1,13 @@
+package com.ace.demoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnildemoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnildemoapiApplication.class, args);
+	}
+
+}
---
+++ FNILDEMOAPI/src/test/java/com/ace/demoapi/FnildemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnildemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
