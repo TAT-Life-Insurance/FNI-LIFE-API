ba69a4f08e2149eec580d232b3aeafcf7763e283
Aung Htet Paing <aunghtetpaing@hotmail.com>
2019-11-01 10:30:29 +0630

    Bank update

--- FNILDEMOAPI/src/main/java/com/ace/demoapi/common/UserRecorder.java
+++ FNILDEMOAPI/src/main/java/com/ace/demoapi/common/UserRecorder.java
@@ -6,7 +6,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import lombok.Data;
-@Data
+
 public class UserRecorder {
 	private String createdUserId;
 	@Temporal(TemporalType.TIMESTAMP)
--- FNILDEMOAPI/src/main/java/com/ace/demoapi/modal/Bank.java
+++ FNILDEMOAPI/src/main/java/com/ace/demoapi/modal/Bank.java
@@ -2,6 +2,7 @@ package com.ace.demoapi.modal;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,6 +10,7 @@ import javax.persistence.Version;
 
 import com.ace.demoapi.common.UserRecorder;
 
+@Entity
 public class Bank {
 
 	@Id
