a2749d6fd129389f838bbbe2fed37d6d1abe79de
Aung Htet Paing <aunghtetpaing@hotmail.com>
2019-11-01 11:59:07 +0630

    application.properties and township update by thihakyaw

--- FNILDEMOAPI/src/main/java/com/ace/demoapi/modal/Township.java
+++ FNILDEMOAPI/src/main/java/com/ace/demoapi/modal/Township.java
@@ -1,6 +1,7 @@
 package com.ace.demoapi.modal;
 
 import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,6 +12,11 @@ import javax.persistence.Version;
 
 import com.ace.demoapi.common.UserRecorder;
 
+import lombok.Data;
+
+
+@Data
+@Entity
 public class Township {
 
 	@Id
@@ -32,83 +38,7 @@ public class Township {
 	@Version
 	private int version;
 
-	public Township() {
-	}
-
-	public Township(String id, String name, String shortName, String code, String description, District district, int version) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.shortName = shortName;
-		this.code = code;
-		this.description = description;
-		this.district = district;
-		this.version = version;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getShortName() {
-		return shortName;
-	}
-
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
-
-	public String getCode() {
-		return code;
-	}
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public UserRecorder getRecorder() {
-		return recorder;
-	}
-
-	public void setRecorder(UserRecorder recorder) {
-		this.recorder = recorder;
-	}
-
-	public District getDistrict() {
-		return district;
-	}
-
-	public void setDistrict(District district) {
-		this.district = district;
-	}
-
-	public int getVersion() {
-		return version;
-	}
-
-	public void setVersion(int version) {
-		this.version = version;
-	}
+	
 
 	public String getFullTownShip() {
 		String fullAddress = name;
